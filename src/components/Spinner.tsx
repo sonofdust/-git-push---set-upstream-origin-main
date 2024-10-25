059505b8dbbd5c7477d7c84dea94836b52d46d93
sonofdust <sonofdust@gmail.com>
2024-10-25 10:50:16 -0600

    Spinner change.

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Spinner = () => (
-    <div role="status" className="flex items-center justify-center">
+    <div role="status" className="flex items-center justify-center h-screen bg-gray-100">
         <svg
             aria-hidden="true"
-            className="w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            className="w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
